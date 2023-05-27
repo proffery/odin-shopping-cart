3b303bc3288b113aa7ab405de80841e8934b6ad4
Dmitry Shamko <proffery@gmail.com>
2023-05-27 14:13:56 +0300

    shop page

--- src/components/RouteSwitch/RouteSwitch.jsx
+++ src/components/RouteSwitch/RouteSwitch.jsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { useState } from 'react'
 import styles from './RouteSwitch.module.css'
 import Home from '../screens/home/Home'
 import Shop from '../screens/shop/Shop'
 import Contact from '../screens/contact/Contact'
 import Cart from '../Cart/Cart'
 import imgCart from '../../assets/img/cart-variant.svg'
-import { useState } from 'react'
+import products from '../../products.json'
 
 const RouteSwitch = () => {
-  const [itemsInCard, setItemsInCard] = useState(0)
+  const [productList, setProductList] = useState(eval(products))
+  const [itemsInCard, setItemsInCard] = useState([])
   const [cartVisibility, setCartVisibility] = useState('hidden')
-
   const openCard = () => {
     cartVisibility === 'hidden' ? setCartVisibility('visible') : setCartVisibility('hidden')
     
@@ -33,8 +34,8 @@ const RouteSwitch = () => {
           </ul>
           <div className={styles.cart} onClick={openCard}>
             <img className={styles.cartImg} src={imgCart} alt='Cart'></img>
-            {itemsInCard > 0 && (
-              <div className={styles.cartNum}>{itemsInCard}</div>
+            {itemsInCard.length > 0 && (
+              <div className={styles.cartNum}>{itemsInCard.length}</div>
             )}
             <div className={styles.bar} style={{
               visibility: cartVisibility,
@@ -45,7 +46,8 @@ const RouteSwitch = () => {
         </div>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/shop' element={<Shop />} />
+          <Route path='/shop' element={<Shop prop={{productList}}/>} />
+          {/* <Route path='/shop/:id' element={<Shop prop={{productList}}/>} /> */}
           <Route path='/contact' element={<Contact />} />
         </Routes>
       </BrowserRouter>
--- src/components/screens/shop/Shop.jsx
+++ src/components/screens/shop/Shop.jsx
@@ -1,10 +1,30 @@
 
-import styles from "./Shop.module.css"
+import { Link } from 'react-router-dom'
+import styles from './Shop.module.css'
+import { useState } from 'react'
 
-const Shop = () => {
+
+const Shop = (prop) => {
+    const [categories, setCategories] = useState([])
+    prop.prop.productList.forEach(product => {
+        categories.includes(product.category) ? (console.log('category '+ product.category + ' exist')) : (setCategories([...categories, product.category]))
+    })
+    console.log(categories)
     return (
         <div className={styles.container}>
-            Shop
+            <h1 className={styles.header}>Categories</h1>
+            <div className={styles.categories}>
+                {categories.map(category =>
+                    <div key={'cat' + categories.indexOf('category')} className={styles.category}>
+                        <Link  to={'/shop/' + category}>
+                            <div className={styles.imgHolder}>
+                                <img className={styles.img} src={'/assets/img/category/' + category + '.jpg'} alt={category} />
+                            </div>
+                            <div className={styles.name}>{category}</div>
+                        </Link>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
